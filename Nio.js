6105ed5479950b91ae7a7605077a152c79bf575d
Stanley Purwahariyanto <stanleypurwaharianto@yahoo.com>
2018-02-13 14:24:48 +0700

    Adding a few new feature

--- Nio.js
+++ Nio.js
@@ -46,6 +46,107 @@ bot.on('message', function (user, userID, channelID, message, evt) {
        
         args = args.splice(1);
         switch(cmd) {
+            case 'hello':
+            var RNG = Math.floor((Math.random() * 11) + 1);
+            switch(RNG)
+            {
+                case 1:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'The melody that reside in your heart, I quite like it.'
+                })
+                ;
+                break;
+                case 2:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'I really cannot stand the melody of that lizard, it is really noisy...'
+                })
+                ;
+                break;
+                case 3:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'The Nine Realm Harp has a very beatiful tune, it is kinda similar to your melody. I like it.'
+                })
+                ;
+                break;
+                case 4:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'If I heard an unpleasant Melody, I wanna put a stop to it right away.'
+                })
+                ;
+                break;
+                case 5:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'I am nio, one of the eternals. I wanna hear more of your beautiful melody.'
+                })
+                ;
+                break;
+                case 6:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'For the next song, I will match it with danchou'
+                })
+                ;
+                break;
+                case 7:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Let us perform together'
+                })
+                ;
+                break;
+                case 8:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Strahv-san melody is kinda weird, I kinda dislike it\nAh he is a schiebt.'
+                })
+                ;
+                break;
+                case 9:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'When will we performing together again?'
+                })
+                ;
+                break;
+                case 10:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'I will put all of your enemy to sleep, so we can beat it together.'
+                })
+                ;
+                break;
+                case 11:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'The melody of that SaintSword is very slow and lazy, it makes me sleepy.'
+                })
+                ;
+                break;
+            }
+            
+            break;
+            //----------------------------------------
+            case 'aww':
+            bot.sendMessage({
+                to: channelID,
+                message: 'https://gbf.wiki/images/f/f3/Stamp81.png'
+            })
+            ;
+            break;
+            //----------------------------------------
+            case 'kono':
+            bot.sendMessage({
+                to: channelID,
+                message: 'https://gbf.wiki/images/6/6b/Stamp107.png'
+            })
+            ;
+            break;
+            //----------------------------------------
             case 'dogeza':
             bot.sendMessage({
                 to: channelID,
@@ -53,13 +154,25 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             })
             ;
             break;
+            //------------------------------------------
             case 'Hi':
                 bot.sendMessage({
                     to: channelID,
                     message: 'Danchou-san melody is very soothing'
                 });
             break;
+            //-------------------------------------------------------
             case 'draw':
+            if(channelID==293266129172824064)
+            {
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'You cannot do that here.'
+                });
+            }
+            else{
+
+            
             var RNG = Math.floor((Math.random() * 100) + 1);
             if (RNG==1||RNG==2||RNG==3)
             {
@@ -82,9 +195,21 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     message: 'your melody is as bad as that lizard, you got R'
                 })
             }
+            }   
             break;
+            //-----------------------------------------------------------
             case 'd10':
             var gacha= [];
+            if(channelID==293266129172824064)
+            {
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'You cannot do that here.'
+                });
+            }
+            else
+            {
+
             for(i=0;i<9;i++)
             {
             var RNG = Math.floor((Math.random() * 100) + 1);
@@ -114,7 +239,18 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 to: channelID,
                 message: `Your Melody giving you these\n${gacha[0]}\n${gacha[1]}\n${gacha[2]}\n${gacha[3]}\n${gacha[4]}\n${gacha[5]}\n${gacha[6]}\n${gacha[7]}\n${gacha[8]}\n${gacha[9]}`
             })
+        }
+            
             break;
+            //-------------------------------------------------------
+            case 'help':
+            bot.sendMessage({
+                to: channelID,
+                message: '**these are the available command for me**\n\n!draw(simulate 1 premium draw)\n!d10(simulate 10 part premium draw)\n!Hi(praising my creator danchou!)\n!hello(I will speak)\n!dogeza(beaDogezaSticker)\n!kono(drangPoke2sticker)\n!aww(drangAwwSticker)'
+            })
+            ;
+            break;
+            
          }
      }
 });
